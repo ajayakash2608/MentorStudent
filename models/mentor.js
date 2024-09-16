ce841461faf341f66a48a414e785e83ed16ca252
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-16 11:52:06 +0530

    Update mentor.js

--- models/mentor.js
+++ models/mentor.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
-const MentorSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }]
+const mentorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    students: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student'
+    }]
 });
 
-module.exports = mongoose.model('Mentor', MentorSchema);
+module.exports = mongoose.model('Mentor', mentorSchema);
