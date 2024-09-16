872e41bbf0fd73c47d0106e25baf4ba9bd3eb986
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-16 11:52:23 +0530

    Update student.js

--- models/student.js
+++ models/student.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
-const StudentSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    mentor: { type: mongoose.Schema.Types.ObjectId, ref: 'Mentor' },
-    previousMentor: { type: mongoose.Schema.Types.ObjectId, ref: 'Mentor' }
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mentor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Mentor',
+        default: null,
+    }
 });
 
-module.exports = mongoose.model('Student', StudentSchema);
+module.exports = mongoose.model('Student', studentSchema);
